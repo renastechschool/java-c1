73fa9aa707bebdf8004cfc8a255190f97ffcf53a
Orion Wolf-Hubbard <owolfhu1@gmail.com>
2021-02-11 23:52:43 -0500

    lesson one

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // This is a comment, this will not effect the code.
+
+        /*
+            This is also a comment,
+            we can leave notes to
+            ourselves and others
+            in comments like this.
+         */
+
+    }
+}
