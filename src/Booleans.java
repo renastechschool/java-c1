4bd7c244627b7ec7eabc3636f1ff9b10214bbca4
Orion Wolf-Hubbard <owolfhu1@gmail.com>
2021-02-13 12:56:19 -0500

    Booleans first half

---
+++ src/Booleans.java
@@ -0,0 +1,68 @@
+@SuppressWarnings("all")
+public class Booleans {
+    public static void main(String[] args) {
+//        String myString = "my string";
+//        char myChar = 's';
+//        int myInt = 1;
+//        double myDouble = 1.1;
+//        boolean myTrueBoolean = true;
+//        boolean myBoolean = myChar != 's'; // false
+//
+//        // !    "not"
+//        // !=   "not equals"
+//
+//        boolean a = myBoolean || false; // false
+//        boolean b = !myTrueBoolean; // false
+//        boolean c = !true || !!false; // false
+//        boolean d = !true == !!false; // true
+//
+//        System.out.println("The boolean value of a is: " + a);
+//        System.out.println("The boolean value of b is: " + b);
+//        System.out.println("The boolean value of c is: " + c);
+//        System.out.println("The boolean value of d is: " + d);
+//
+//        //                  f    f    t
+//        boolean myNewBool = a || c || d; // true
+//
+//        int int1 = 3;
+//        int int2 = 11;
+//        int int3 = 5;
+//        boolean myResult = ( (!false || 1 > int1) || (!true && !!false) ) == false ; // false
+
+
+        boolean myCondition = false;
+
+        // java keywords: true false
+
+        /*
+        if (exprsion that we check is true) {
+
+          do this code block if the expretion is true
+
+        }
+         */
+
+        int orionsInt = 12;
+        int erimsInt = 15;
+        boolean myBool = false;
+        // start with one 'if'
+        // then as many 'else if' as you need
+        // then can end with one 'else'
+
+        if (orionsInt > erimsInt || !myBool) {
+            System.out.println("!!!!");
+        } else if (orionsInt == erimsInt) {
+            System.out.println("????");
+        } else {
+            System.out.println("####");
+        }
+
+        if (1 + 1 == 2) {
+            System.out.println("1 + 1 is 2");
+        } else {
+            System.out.println("1 + 1 is not 2");
+        }
+
+
+    }
+}
