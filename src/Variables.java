ea3b723cf3cca73ced4231f3153c6c142b94c1f5
Orion Wolf-Hubbard <owolfhu1@gmail.com>
2021-02-12 21:46:26 -0500

    variables class

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        String text = "this is text";
+        int wholeNumber = 5;
+        double decimal = 1.5;
+        char letter = 'h';
+
+        String message = "The text is " + text + " and the numbers are " + wholeNumber + " and " + decimal + " the letter is " + letter;
+        System.out.println(message);
+
+    }
+
+}
